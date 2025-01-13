d3973e44011559e89e315af5ab872cc5d8d4af52
coderemon24 <mdemonahmed2021@gmail.com>
2025-01-13 18:29:34 +0600

    creating pages

---
+++ app/Http/Livewire/Admin/Pages/CreatePageComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Admin\Pages;
+
+use Livewire\Component;
+
+class CreatePageComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.pages.create-page-component');
+    }
+}
